f4dd4096cc8fda108f08a04c59a6f499d0f605d7
Stercy Mutuku <mutukustercy93@gmail.com>
2025-06-10 15:29:57 +0300

    Finding a value of an object using find() method

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+
+
+function superbowlWin(recordarray){
+
+    let foundWinner = recordarray.find(finder)
+
+    return foundWinner ? foundWinner.year : undefined
+
+}
+function finder(currentValue){
+    // currentValue:  {year: "2015", result: "W"}
+    let targetrecord =  currentValue.result == "W"
+    return targetrecord
+}
+
+const record = [
+  { year: "2015", result: "W"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"}
+]
+console.log(superbowlWin(record))
